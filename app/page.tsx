5ff6680d1b911db3e38b1db8f800f72f4f680d4c
Neirel <neirelzapatos@gmail.com>
2024-10-08 11:11:33 -0700

    Got environment vairables working

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 import { StrictMode } from "react";
+import env from "dotenv";
+
+// allows for use of environment variables
+// ex: to get an environment variable called TESTING you would type process.env.TESTING
+env.config();
 
 export default function Home() {
   return (
