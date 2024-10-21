eadc097fef0470ae0c91023568829cd2862996b9
alexander-bass <145721183+alexander-bass@users.noreply.github.com>
2024-10-20 18:36:20 -0700

    S26W-89 Create class catalog page template (#22)
    * Added name to README
    
    * Created basic template for class catalog page, including header, footer, simple navbar, and body. Placed each individual component in it's own file in /class-catalog/test-components folder. Also modified the global layout file to include the daisyUI Winter theme.
    
    * Update layout.tsx
    
    removed daisyUI theme

---
+++ app/class-catalog/page.tsx
@@ -0,0 +1,21 @@
+import Hero from "./test-components/Hero";
+import ClassCard from "./test-components/ClassCard";
+
+export default function Page() {
+    return (
+      <div className="flex w-full flex-col border-opacity-50">
+        <Hero />
+        <div className="divider"></div>
+        <div className="flex w-full">
+          <ClassCard />
+          <div className="divider divider-horizontal"></div>
+          <ClassCard />
+          <div className="divider divider-horizontal"></div>
+          <ClassCard />
+          <div className="divider divider-horizontal"></div>
+          <ClassCard />
+        </div>
+        <div className="divider"></div>
+      </div>
+    );
+  }
