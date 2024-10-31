cd6f6259d78371d29e00a1a9aff4a84fc4a9a622
chrubble <47755013+notchrubble@users.noreply.github.com>
2024-10-30 17:03:53 -0700

    S26W-104 As an owner I want to be able to log into my admin dashboard (#37)
    * add preliminary testing for authentication with next-auth
    - added auth/[...nextauth] folder
    - added route.ts
    
    * S26W-116 Implement authentication for an Admin account
    - Added middleware.ts
    - Completed authentication functionality for Admin accounts
    - On successful login, will route the Admin to the Dashboard page
    - Fixed the google logo not showing up on the login button
    
    * Updated next-auth dependencies
    - Additionally commented out adapters

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,64 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+//import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import { MongoClient } from "mongodb";
+import Admin from "@/app/models/Admin";
+import bcrypt from "bcrypt";
+import dbConnect from "@/app/lib/dbConnect";
+
+const client = new MongoClient(process.env.MONGODB_URI!);
+const clientPromise = client.connect();
+
+export const authOptions: NextAuthOptions = {
+  //adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials, req) {
+        // if (!credentials?.email || !credentials.password) return null;
+        if (!credentials?.email || !credentials.password){
+          throw new Error("Email and password are required.");
+        }
+
+        await dbConnect();
+
+        const admin = await Admin.findOne({
+          email: credentials.email,
+        });
+        
+        if (!admin || !admin.hashed_password){
+          throw new Error("Incorrect Username or password");
+        }
+
+        // console.log("User password:", credentials.password);
+        // console.log("Stored hashed password:", admin.hashed_password);
+
+
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          admin.hashed_password
+        );
+
+        return passwordsMatch ? admin : null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  theme: {
+    colorScheme: "light", // Sets light mode
+  },
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
