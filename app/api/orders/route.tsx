83ed7216c95020a15a4a3ae1029349bed905b47b
Neirel <neirelzapatos@gmail.com>
2025-01-29 14:47:42 -0800

    Finicial Analytics now pull from orders in the database

--- app/api/orders/route.tsx
+++ app/api/orders/route.tsx
@@ -2,12 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import schema from './schema';
 import dbConnect from '@/app/lib/dbConnect';
 import Order from '@/app/models/Order';
+import Item from '@/app/models/Item';
+import Course from '@/app/models/Course';
 
 // Example of JSON to send to this api endpoint
 // {
 //     "customer_id": "671ef81602098f94990d0c0f",
 //     "product_items": ["671d62eb17058eb97e7c7f4e", "671d630017058eb97e7c7f52"],
-//     "total_amount": 150.60,
 //     "shipping_method": "Ground",
 //     "payment_method": "Credit Card",
 //     "order_status": "pending",
@@ -35,6 +36,8 @@ export async function POST(request: NextRequest) {
         await dbConnect();
 
         const body = await request.json();
+        // for testing purposes
+        // body.order_date = new Date(body.order_date);
         body.order_date = new Date();
         const validation = schema.safeParse(body);
 
@@ -42,10 +45,20 @@ export async function POST(request: NextRequest) {
             return NextResponse.json(validation.error.errors, { status: 400 })
         }
 
-        // let total_amount = 0;
-        // for(let i = 0; i < body.product_items.length; i++) {
-            
-        // }
+        let total_amount = 0;
+        for(let i = 0; i < body.product_items.length; i++) {
+            const productId = body.product_items[i];
+            const item = await Item.findById(productId);
+            total_amount += item.price;
+        }
+
+        for(let i = 0; i < body.course_items.length; i++) {
+            const courseId = body.course_items[i];
+            const course = await Course.findById(courseId);
+            total_amount += course.price;
+        }
+
+        body.total_amount = total_amount;
 
         const newOrder = new Order(body);
         await newOrder.save();
