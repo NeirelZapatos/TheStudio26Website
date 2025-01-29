79067ac9a289035ce15d3cd007df3e3c2b8e2d4c
Alexander Bass <alexbass@csus.edu>
2025-01-28 17:48:38 -0800

    create newsletter branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: [process.env.S3_BUCKET_DOMAIN, 'picsum.photos', 'www.stockvault.net', 'images.freeimages.com', 'media.istockphoto.com'],
+      domains: ['process.env.S3_BUCKET_DOMAIN', 'picsum.photos', 'www.stockvault.net', 'images.freeimages.com', 'media.istockphoto.com'],
     },
   };
   
