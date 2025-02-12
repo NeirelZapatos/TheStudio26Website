eb502644e3cb669945b2061fd7b37ee2ce7d650c
chrubble <47755013+notchrubble@users.noreply.github.com>
2025-02-11 22:15:25 -0800

    Fixed products not showing up on store page - Conditionally, need a new way to show the price on the product cards

---
+++ app/(CustomerPages)/StoreSearch/Components/Filters.tsx
@@ -0,0 +1,306 @@
+const SUBCATEGORIES = [
+  { name: "Stones", selected: false, href: "#" },
+  { name: "Jewelry", selected: false, href: "#" },
+  { name: "Supplies", selected: false, href: "#" },
+];
+
+const COLOR_FILTERS = {
+  id: "color",
+  name: "Color",
+  options: [
+    { value: "white", label: "White" },
+    { value: "beige", label: "Beige" },
+    { value: "silver", label: "Silver" },
+    { value: "gold", label: "Gold" },
+    { value: "black", label: "Black" },
+    { value: "blue", label: "Blue" },
+    { value: "green", label: "Green" },
+    { value: "red", label: "Red" },
+  ] as const,
+};
+
+const MATERIAL_FILTERS = {
+  id: "material",
+  name: "Material",
+  options: [
+    { value: "gold", label: "Gold" },
+    { value: "copper", label: "Copper" },
+    { value: "silver", label: "Silver" },
+  ] as const,
+};
+
+const SIZE_FILTERS = {
+  id: "size",
+  name: "Size",
+  options: [
+    { value: "S", label: "S" },
+    { value: "M", label: "M" },
+    { value: "L", label: "L" },
+  ] as const,
+};
+
+const PRICE_FILTERS = {
+  id: "price",
+  name: "Price",
+  options: [
+    { value: [0, 500], label: "Any price" },
+    { value: [0, 49], label: "$0 - $49" },
+    { value: [50, 99], label: "$50 - $99" },
+    { value: [100, 500], label: "$100 - $500" },
+  ] as const,
+};
+
+const DEFAULT_CUSTOM_PRICE = [0, 500] as [number, number];
+
+interface FilterProps {
+  filter: FilterState;
+  setFilter: React.Dispatch<React.SetStateAction<FilterState>>;
+}
+
+interface FilterState {
+  sort: string;
+  category: string;
+  color: string[];
+  material: string[];
+  size: string[];
+  price: {
+    isCustom: boolean;
+    range: [number, number];
+  };
+}
+
+const Filters = ({ filter, setFilter }: FilterProps) => {
+  console.log(filter);
+
+  const handleCategoryChange = (category: string) => {
+    setFilter((prev) => ({
+      ...prev,
+      category, // Update the selected category
+    }));
+  };
+
+  const applyArrayFilter = ({
+    category,
+    value,
+  }: {
+    category: keyof Pick<FilterState, "color" | "material" | "size">;
+    value: string;
+  }) => {
+    const isFilterApplied = filter[category].includes(value);
+    setFilter((prev: FilterState) => ({
+      ...prev,
+      [category]: isFilterApplied
+        ? prev[category].filter((v: string) => v !== value)
+        : [...prev[category], value],
+    }));
+  };
+
+  const minPrice = Math.min(filter.price.range[0], filter.price.range[1]);
+  const maxPrice = Math.max(filter.price.range[0], filter.price.range[1]);
+
+  return (
+    <div className="hidden lg:block overflow-y-auto">
+      <ul className="space-y-4 border-b border-gray-200 pb-6 text-md font-medium text-gray-900">
+        <li>
+          <button
+            onClick={() => handleCategoryChange("all")}
+            className={`w-full text-left ${
+              filter.category === "all"
+                ? "text-gray-900"
+                : "text-gray-500 hover:text-gray-700"
+            }`}
+          >
+            All Items
+          </button>
+        </li>
+        {SUBCATEGORIES.map((category) => (
+          <li key={category.name}>
+            <button
+              onClick={() => handleCategoryChange(category.name)}
+              className={`w-full text-left ${
+                filter.category === category.name.toLowerCase()
+                  ? "text-gray-900"
+                  : "text-gray-500 hover:text-gray-700"
+              }`}
+            >
+              {category.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {/* Accordion */}
+      {/* Color Filters */}
+      <div className="collapse collapse-arrow">
+        <input type="checkbox" name="my-accordion-2"/>
+        <div className="collapse-title text-med text-gray-400 hover:text-gray-500">
+          <span className="font-medium text-gray-900">Color</span>
+        </div>
+        <div className="collapse-content animate-none">
+          <ul className="space-y-4">
+            {COLOR_FILTERS.options.map((option, optionIdx) => (
+              <li key={option.value} className="flex items-center">
+                <input
+                  type="checkbox"
+                  id={`color-${optionIdx}`}
+                  onChange={() => {
+                    applyArrayFilter({
+                      category: "color",
+                      value: option.value,
+                    });
+                  }}
+                  checked={filter.color.includes(option.value)}
+                  className="checkbox checkbox-sm"
+                />
+                <label
+                  htmlFor={`color-${optionIdx}`}
+                  className="px-2 text-sm text-gray-600"
+                >
+                  {option.label}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      {/* Prices Filter */}
+      <div className="collapse collapse-arrow">
+        <input type="checkbox" name="my-accordion-2" />
+        <div className="collapse-title text-med text-gray-400 hover:text-gray-500">
+          <span className="font-medium text-gray-900">Price</span>
+        </div>
+        <div className="collapse-content animate-none">
+          <ul className="space-y-4">
+            {PRICE_FILTERS.options.map((option, optionIdx) => (
+              <li key={option.label} className="flex items-center">
+                <input
+                  type="radio"
+                  id={`price-${optionIdx}`}
+                  onChange={() => {
+                    setFilter((prev: FilterState) => ({
+                      ...prev,
+                      price: {
+                        isCustom: false,
+                        range: [...option.value],
+                      },
+                    }));
+                  }}
+                  checked={
+                    !filter.price.isCustom &&
+                    filter.price.range[0] === option.value[0] &&
+                    filter.price.range[1] === option.value[1]
+                  }
+                  className="radio"
+                />
+                <label
+                  htmlFor={`price-${optionIdx}`}
+                  className="px-2 text-sm text-gray-600"
+                >
+                  {option.label}
+                </label>
+              </li>
+            ))}
+            {/* <li className="flex jsutify-center flex-col gap-2">
+              <div className="flex justify-between">
+                <p className="font-medium">Price</p>
+                <div>
+                  ${minPrice.toFixed(0)} - ${maxPrice.toFixed(0)}
+                </div>
+              </div>
+              <div className="flex gap-2">
+                <input
+                  type="range"
+                  min={0}
+                  max={500}
+                  value={maxPrice}
+                  onChange={(e) => {
+                    const newMax = Number(e.target.value);
+                    setFilter((prev: FilterState) => ({
+                      ...prev,
+                      price: {
+                        ...prev.price,
+                        range: [prev.price.range[0], newMax],
+                      },
+                    }));
+                  }}
+                  className="range range-sm"
+                />
+              </div>
+            </li> */}
+          </ul>
+        </div>
+      </div>
+
+      {/* Materials Filter */}
+      <div className="collapse collapse-arrow">
+        <input type="checkbox" name="my-accordion-2" />
+        <div className="collapse-title text-med text-gray-400 hover:text-gray-500">
+          <span className="font-medium text-gray-900">Material</span>
+        </div>
+        <div className="collapse-content animate-none">
+          <ul className="space-y-4">
+            {MATERIAL_FILTERS.options.map((option, optionIdx) => (
+              <li key={option.value} className="flex items-center">
+                <input
+                  type="checkbox"
+                  id={`material-${optionIdx}`}
+                  onChange={() => {
+                    applyArrayFilter({
+                      category: "material",
+                      value: option.value,
+                    });
+                  }}
+                  checked={filter.material.includes(option.value)}
+                  className="checkbox checkbox-sm"
+                />
+                <label
+                  htmlFor={`material-${optionIdx}`}
+                  className="px-2 text-sm text-gray-600"
+                >
+                  {option.label}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      {/* Sizes Filter */}
+      <div className="collapse collapse-arrow">
+        <input type="checkbox" name="my-accordion-2" />
+        <div className="collapse-title text-med text-gray-400 hover:text-gray-500">
+          <span className="font-medium text-gray-900">Size</span>
+        </div>
+        <div className="collapse-content animate-none">
+          <ul className="space-y-4">
+            {SIZE_FILTERS.options.map((option, optionIdx) => (
+              <li key={option.value} className="flex items-center">
+                <input
+                  type="checkbox"
+                  id={`size-${optionIdx}`}
+                  onChange={() => {
+                    applyArrayFilter({
+                      category: "size",
+                      value: option.value,
+                    });
+                  }}
+                  checked={filter.size.includes(option.value)}
+                  className="checkbox checkbox-sm"
+                />
+                <label
+                  htmlFor={`size-${optionIdx}`}
+                  className="px-2 text-sm text-gray-600"
+                >
+                  {option.label}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
