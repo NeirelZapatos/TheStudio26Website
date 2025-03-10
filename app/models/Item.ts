650ba028e780b8de57b25fd62190f04b2d5ef56c
Neirel <neirelzapatos@gmail.com>
2025-03-10 02:01:30 -0700

    #53 As a developer, I want to enhance our database with more fields allowing for more information to customer and owner (Part 1)

--- app/models/Item.ts
+++ app/models/Item.ts
@@ -50,6 +50,41 @@ const itemSchema:Schema = new mongoose.Schema({
         default: 0,
         min: 0,
     },
+    
+    // jewlery fields
+    jewelry_type: {
+        type: String,
+    },
+    metal_type: {
+        type: String,
+    },
+    metal_purity: {
+        type: String
+    },
+    metal_finish: {
+        type: String
+    },
+    plating: {
+        type: String
+    },
+    ring_size: {
+        type: Number
+    },
+    gauge: {
+        type: Number
+    },
+    carat_weight: {
+        type: Number
+    },
+    setting_type: {
+        type: String
+    },
+    stone_arrangement: {
+        type: String
+    },
+    customization_options: {
+        type: String
+    }
 });
 
 const Item = mongoose.models.Item || mongoose.model<IItem>('Item', itemSchema);
