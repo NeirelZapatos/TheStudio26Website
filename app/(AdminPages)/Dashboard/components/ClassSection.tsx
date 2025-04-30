d349af3a58d4d0fcf7754d6d7a7cd2dc43886baa
Ray Laciste <raylaciste@Rays-MacBook-Air-3.local>
2025-04-30 11:43:37 -0700

    moved clickable part from text to li

--- app/(AdminPages)/Dashboard/components/ClassSection.tsx
+++ app/(AdminPages)/Dashboard/components/ClassSection.tsx
@@ -465,8 +465,9 @@ export default function Page() {
                   <li
                     key={index}
                     className="p-2 flex justify-between items-center hover:bg-gray-200 cursor-pointer"
+                    onClick={() => loadTemplate(index.toString())}
                   >
-                    <span onClick={() => loadTemplate(index.toString())}>
+                    <span>
                       {template.name}
                     </span>
                     <button
